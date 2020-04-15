f5aa198d5b57dbb91581f4d9e0796d9f373bae03
徐冬平 <xudongping@xudongpingdeMacBook-Air.local>
2020-04-15 20:43:20 +0800

    链表

--- 数据结构与算法/数据结构/LinkedList/doubleList/main.go
+++ 数据结构与算法/数据结构/LinkedList/doubleList/main.go
@@ -1,5 +1,121 @@
 package main
 
+import (
+	"fmt"
+)
+
+type Node struct {
+	Data int
+	Prev *Node
+	Next *Node
+}
+
+
+type DeLinkList struct {
+	head *Node //由于链表任何操作都需要从头部开始，因此这里定义一个链表头部属性
+}
+
+//只需要判断首部节点是否为空
+func (self *DeLinkList) IsEmpty() bool{
+	return self.head == nil
+}
+
+//链表长度
+func (self *DeLinkList) Length() int{
+	count := 0
+	head := self.head
+	for head != nil{
+		count += 1
+		head = head.Next
+	}
+	return count
+}
+
+//从头遍历
+func (self *DeLinkList) Range()  {
+	head := self.head
+	for head != nil{
+		fmt.Println("当前元素",head.Data)
+		head = head.Next
+	}
+}
+
+//从头部添加节点
+func (self *DeLinkList) Add(value int){
+	newNode := &Node{Data:value}
+	if self.head == nil{
+		self.head = newNode
+	}else {
+		newNode.Next = self.head
+		self.head.Prev = newNode
+		self.head = newNode
+	}
+}
+
+//从尾部添加元素
+func (self *DeLinkList) Append(value int){
+	head := self.head
+	for head.Next != nil{
+		head = head.Next
+	}
+	newNode := &Node{Data:value}
+	newNode.Prev = head
+	head.Next = newNode
+}
+
+//删除任意位置的元素，todo 需要处理：删除的元素在头部，中部，尾部三种情况
+func (self *DeLinkList) Delete(value int){
+	head := self.head
+	//删除头部
+	if head.Data == value{
+		self.head = head.Next
+	}else {
+		for head != nil{
+			if head.Data == value{
+				//修改这个元素之前的一个元素Next指针
+				head.Prev.Next = head.Next
+				if head.Next != nil{
+					head.Next.Prev = head.Prev
+				}
+				break
+			}else {
+				head = head.Next
+			}
+		}
+
+	}
+}
+
+// 在任意位置插入数据 todo 需要考虑插入数据位于 首部, 尾部,中间的处理情况
+func (self *DeLinkList) Insert(index,value int){
+	if index <= 0{
+		self.Add(value)
+	}else if index > self.Length(){
+		self.Append(value)
+	}else{
+		//插入数据在中间的情况,需要找到index之前的元素修改指针
+		head := self.head
+		count := 0
+		for count < index{
+			count += 1
+			head = head.Next
+		}
+		newNode := &Node{Data:value}
+		newNode.Prev = head.Prev
+		newNode.Next = head
+		head.Prev.Next = newNode
+		head.Prev = newNode
+	}
+}
+
 func main() {
-	
+	delink := &DeLinkList{}
+	delink.Add(10)
+	delink.Add(15)
+	delink.Append(8)
+	delink.Append(66)
+	delink.Append(15)
+	delink.Insert(2,88)
+	fmt.Println("长度",delink.Length())
+	delink.Range()
 }
--- 数据结构与算法/数据结构/LinkedList/singleList/main.go
+++ 数据结构与算法/数据结构/LinkedList/singleList/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Elem int
 
 type LinkNode struct {
@@ -13,6 +15,77 @@ func New() *LinkNode{
 	return &LinkNode{0,nil}
 }
 
+func (head *LinkNode) Insert(i int,e Elem) bool{
+	p := head
+	j := 1
+	for p != nil && j < i{
+		p = p.Next
+		j++
+	}
+	if p == nil || j > 1{
+		fmt.Println("pls check i:",i)
+		return  false
+	}
+	s := &LinkNode{Data:e}
+	s.Next = p.Next
+	p.Next = s
+	return true
+}
+
+//遍历链表
+func(head *LinkNode) Traverse(){
+	point := head.Next
+	for point != nil{
+		fmt.Println(point.Data)
+		point = point.Next
+	}
+	fmt.Println("------done------")
+}
+
+//删除链表中第i个节点，复杂度为o（n）
+func (head *LinkNode) Delete(i int) bool{
+	p := head
+	j := 1
+	for (p != nil && j < i){
+		p = p.Next
+		j++
+	}
+	if p == nil || j > i{
+		fmt.Println("pls check i:",i)
+		return false
+	}
+
+	p.Next = p.Next.Next
+	return true
+}
+
+//获取链表中的第i个元素,复杂度为o(n)
+func (head *LinkNode) Get(i int) Elem{
+	p := head.Next
+	for j := 1;j < 1;i++{
+		if p == nil{
+			//表示返回错误
+			return -100001
+		}
+		p = p.Next
+	}
+	return p.Data
+}
+
+
 func main() {
-	
+	linkedList := New()
+	linkedList.Insert(1,9)
+	linkedList.Insert(1,99)
+	linkedList.Insert(1,999)
+	linkedList.Insert(1,9999)
+	linkedList.Insert(1,99999)
+	linkedList.Insert(1,999999)
+	linkedList.Traverse()
+
+	linkedList.Delete(4)
+	linkedList.Traverse()
+
+	e := linkedList.Get(4)
+	fmt.Println(e)
 }
