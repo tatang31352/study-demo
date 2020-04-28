b012910fbbe1180be31330f473fc26818b7c865f
xpFelix <1092702994@qq.com>
2020-04-28 11:05:28 +0800

    网络协议

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+)
+var ch2 =  make(chan bool)
+
 func main() {
-	$END$
+	ch := make(chan int)
+	go write(ch)
+	go read(ch)
+	fmt.Println(<-ch2)
+}
+
+
+func write(ch chan int){
+	for i:= 0;i < 10;i++{
+		ch <- i
+	}
+	close(ch)
+}
+
+func read(ch chan int) {
+
+	for{
+		v,ok := <-ch
+		if ok{
+			fmt.Println(v)
+		}else{
+			ch2 <- true
+		}
+	}
+
 }
